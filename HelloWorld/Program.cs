45bb923a086eda2625c7b457e6469bcd0bcab9a2
Raquel Joseph Abaya <rjabaya0110@gmail.com>
2022-08-03 13:31:32 -0400

    Modified the Hello World Program

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,3 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
+/*
+    There are three control structures in C# Programming: Sequence, Decisions and Repititions
+ */
+
 Console.WriteLine("Hello, World!");
+
+
+
+
+// Test Modification
 
